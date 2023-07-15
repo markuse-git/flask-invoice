d00638dac951951902e000c02baa301730f4e878
Markus <markus.eichelhardt@web.de>
2023-07-15 20:34:40 +0200

    flask-security eingebaut und templates gestyled

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
 from flask import Flask
 from flask_migrate import Migrate
 from flask_admin import Admin
+from flask_security import Security
 
 from views import blp as ViewsBlueprint
 from admin import ClientView, InvoiceView, ItemView
-from models import ClientModel, InvoiceModel, ItemModel
+from models import ClientModel, InvoiceModel, ItemModel, user_datastore
 
 from db import db
 
@@ -15,13 +16,19 @@ def create_app():
     app.config['SECRET_KEY'] = '123123'
     app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///invoices.db'
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-    app.config['DEBUG'] = True
+    app.config['SECURITY_REGISTERABLE'] = True
+    app.config['SECURITY_PASSWORD_SALT'] = 'mysalt'
+    app.config['SECURITY_SEND_REGISTER_EMAIL'] = False
+    app.config['SECURITY_RECOVERABLE'] = True
+    app.config['SECURITY_CHANGEABLE'] = True
 
     admin = Admin(app, template_mode='bootstrap3')
     # admin = Admin(app, base_template='/templates/admin/master.html')
     admin.add_view(ClientView(ClientModel, db.session))
     admin.add_view(ItemView(ItemModel, db.session))
     admin.add_view(InvoiceView(InvoiceModel, db.session))
+
+    Security(app, user_datastore)
 
     app.register_blueprint(ViewsBlueprint)
 
